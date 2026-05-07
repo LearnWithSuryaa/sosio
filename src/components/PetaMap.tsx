85cb2916528973b16a3ce1eb231bfa2b10f489fe
LearnWithSuryaa <davindra514@gmail.com>
2026-05-07 18:17:50 +0700

    fix: update map tile layer URL and add attribution for improved map rendering

--- src/components/PetaMap.tsx
+++ src/components/PetaMap.tsx
@@ -148,7 +148,10 @@ export default function PetaKomponen({ schoolId, schools }: Props) {
         style={{ background: "#080c14" }}
       >
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          subdomains="abcd"
+          maxZoom={19}
         />
         <MapController schools={schools} targetId={schoolId} />
         
